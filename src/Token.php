5cdbc5af3b3511dd190dedc272e534730f7d13d9
Márcio Almada <marcio3w@gmail.com>
2015-07-28 23:59:59 -0300

    implement Token

---
+++ src/Token.php
@@ -0,0 +1,127 @@
+<?php declare(strict_types=1);
+
+namespace Yay;
+
+class Token implements \JsonSerializable {
+
+    /**
+     * pseudo token types
+     */
+    const
+        EOF = 1010,
+        ANY = 1021,
+        NONE = 1032,
+        MATCH = 1043,
+        OPERATOR = 1054
+    ;
+
+    /**
+     * lookup table used to dump pseudo token types
+     */
+    const TOKENS = [
+        self::EOF => 'EOF',
+        self::ANY => 'ANY',
+        self::NONE => 'NONE',
+        self::MATCH => 'MATCH',
+        self::OPERATOR => 'OPERATOR'
+    ];
+
+    protected
+        $type,
+        $value,
+        $line,
+        $literal = false
+    ;
+
+    function __construct($type, string $value = null, int $line = null) {
+        if (! is_scalar($type)) {
+            throw new YayException("Token type must be int or string.");
+        }
+
+        if (is_string($type)) {
+            if(1 !== mb_strlen($type))
+                throw new YayException("Invalid token type '{$type}'");
+
+            $this->literal = true;
+            $value = $type;
+        }
+
+        $this->type = $type;
+        $this->value = $value;
+        $this->line = $line;
+    }
+
+    function __toString(): string {
+        return (string) $this->value;
+    }
+
+    function __debugInfo() {
+        return [$this->dump()];
+    }
+
+    function dump(): string {
+        $name = $this->name();
+
+        return $this->literal ? "'{$name}'" : "{$name}({$this->value})";
+    }
+
+    function mutate(self $proto) {
+        list($this->type, $this->value) = [$proto->type, $proto->value];
+    }
+
+    function is(/* string|int */ ...$types): bool {
+        return in_array($this->type, $types);
+    }
+
+    function contains($value): bool {
+        return $value === null ?: $this->value === $value;
+    }
+
+    function equals(self $token): bool {
+        return 1 === ($this->is($token->type) & $this->contains($token->value));
+    }
+
+    function name(): string {
+        return
+            ($this->literal)
+                ? $this->type
+                : self::TOKENS[$this->type] ?? token_name($this->type)
+        ;
+    }
+
+    function type() /* : string|int */ {
+        return $this->type;
+    }
+
+    function line(): int {
+        return $this->line ?: 0;
+    }
+
+    function literal() : bool {
+        return $this->literal;
+    }
+
+    static function eof() : self {
+        return new self();
+    }
+
+    static function any() : self {
+        return new self(self::ANY);
+    }
+
+    static function none() : self {
+        return new self(self::NONE);
+    }
+
+    static function match(string $value) : self {
+        return new self(self::MATCH, $value);
+    }
+
+    static function operator(string $value) : self {
+        return new self(self::OPERATOR, $value);
+    }
+
+    function jsonSerialize() {
+        return $this->__toString();
+    }
+}
---
+++ tests/TokenTest.php
@@ -0,0 +1,86 @@
+<?php declare(strict_types=1);
+
+namespace Yay;
+
+/**
+ * @group small
+ */
+class TokenTest extends \PHPUnit_Framework_TestCase {
+    function equalsProvider() : array {
+        return [
+            [
+                [T_STRING, '"yep"'],
+                [T_STRING, '"nope"'],
+                false
+            ],[
+                [T_STRING, '""'],
+                [':'], false],
+            [
+                [T_STRING, '"any"'],
+                [T_STRING],
+                true
+            ],[
+                [T_STRING, "·"],
+                [T_STRING, '·'],
+                true],
+            [
+                [T_STRING, "·"],
+                [T_STRING],
+                true
+            ],[
+                [T_FUNCTION, 'function'],
+                [T_FUNCTION],
+                true
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider equalsProvider
+     */
+    function testEquals($a, $b, bool $assertion) {
+        $token_a = new Token(...$a);
+        $token_b = new Token(...$b);
+        $this->assertEquals(
+            $assertion
+            ,
+            $token_a->equals($token_b)
+            ,
+            "{$token_a->dump()} !== {$token_b->dump()}"
+        );
+    }
+
+    function testIs() {
+        $token = new Token('$');
+        $this->assertTrue($token->is('$'));
+        $this->assertTrue($token->is('$', ':', '!'));
+        $this->assertTrue($token->is('!', '?', ':', '$'));
+        $this->assertFalse($token->is('!', '?', ':'));
+        $this->assertFalse($token->is('$!'));
+
+        $token = new Token(T_STRING);
+        $this->assertFalse($token->is(T_OPEN_TAG, T_YIELD));
+        $this->assertTrue($token->is(T_STRING));
+
+        $token = new Token(T_STRING, '"value"', null);
+
+        $this->assertFalse($token->is(T_OPEN_TAG, T_YIELD));
+        $this->assertTrue($token->is(T_CLOSE_TAG, T_STRING));
+    }
+
+    function testContains() {
+        $token = new Token('$');
+        $this->assertTrue($token->contains('$'));
+        $this->assertFalse($token->contains('!'));
+
+        $token = new Token(T_STRING);
+        $this->assertTrue($token->contains(null));
+        $this->assertFalse($token->contains(''));
+
+        $token = new Token(T_STRING, '"yep"', null);
+        $this->assertTrue($token->contains(null));
+        $this->assertFalse($token->contains('"nope"'));
+        $this->assertTrue($token->contains('"yep"'));
+    }
+
+}
