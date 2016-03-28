fcf8b00cb12aa19262c1f6d4268e8761a361822f
Márcio Almada <marcio3w@gmail.com>
2016-03-27 21:21:37 -0400

    makes temporary yay_compile() suck less

--- yay_compile.php
+++ yay_compile.php
@@ -1,8 +1,14 @@
 <?php declare(strict_types=1);
 
-function yay_compile(string $file) {
-    $source = explode('return \yay_compile(__FILE__); __halt_compiler();', file_get_contents($file));
-    $tmp = tempnam(sys_get_temp_dir(), 'yay'. $file);
-    file_put_contents($tmp, \yay_parse( '<?php' . end($source)));
-    return include "yay://{$tmp}";
+function yay_compile(string $file, string $dir = null) {
+    $source =
+        explode(
+            'return \yay_compile(__FILE__); __halt_compiler();',
+            file_get_contents($file)
+        )[1]
+    ;
+    $tmpfile = ($dir ?: sys_get_temp_dir()) . '/yay' . str_replace(['/', '\\'], '-', $file);
+    file_put_contents($tmpfile, \yay_parse( '<?php' . $source));
+
+    return include "yay://{$tmpfile}";
 }
