3d026a8e9e8b9253cf0799784e274f3f3d47381e
Márcio Almada <marcio3w@gmail.com>
2017-01-27 19:56:24 -0400

    allow optional ast unfold with `·foo ?··· {/* expansion */}` syntax
    cc @assertchris

--- src/Expansion.php
+++ src/Expansion.php
@@ -119,6 +119,8 @@ class Expansion extends MacroMember {
             (
                 rtoken('/^·\w+|···\w+$/')->as('label')
                 ,
+                optional(token('?'))->as('optional')
+                ,
                 operator('···')
                 ,
                 optional
@@ -129,7 +131,8 @@ class Expansion extends MacroMember {
                 braces()->as('expansion')
             )
             ->onCommit(function(Ast $result) use($cg) {
-                $this->lookupContext($result->label, $cg->context, self::E_UNDEFINED_EXPANSION);
+                if (null !== $result->optional)
+                    $this->lookupContext($result->label, $cg->context, self::E_UNDEFINED_EXPANSION);
                 $this->constant = false;
             })
             ,
@@ -198,6 +201,8 @@ class Expansion extends MacroMember {
                     (
                         rtoken('/^·\w+|···\w+$/')->as('label')
                         ,
+                        optional(token('?'))->as('optional')
+                        ,
                         operator('···')
                         ,
                         optional
@@ -211,7 +216,12 @@ class Expansion extends MacroMember {
                 ->onCommit(function(Ast $result)  use($states) {
                     $cg = $states->current();
 
-                    $context = $cg->this->lookupContext($result->label, $cg->context, self::E_UNDEFINED_EXPANSION);
+                    if (null !== $result->optional)
+                        $context = $cg->this->lookupContextOptional($result->label, $cg->context);
+                    else
+                        $context = $cg->this->lookupContext($result->label, $cg->context, self::E_UNDEFINED_EXPANSION);
+
+                    if ($context === null) return;
 
                     $expansion = TokenStream::fromSlice($result->expansion);
                     $delimiters = $result->delimiters;
@@ -324,5 +334,11 @@ class Expansion extends MacroMember {
         }
 
         return $result;
+    }
+
+    private function lookupContextOptional(Token $token, Context $context) /*: Token | []Token*/ {
+        $symbol = (string) $token;
+
+        return $context->get($symbol);
     }
 }
