dc74825cfd07e2b3855ca37cb22a58c614c7eaaa
Márcio Almada <marcio3w@gmail.com>
2019-04-19 09:08:48 -0300

    Revert "Add ParsedTokenStream class and use it for expansion"
    This reverts commit f0d140730983c2e1d78f479cd03855323f468c9f.

--- tests/fixtures/parsers.php
+++ tests/fixtures/parsers.php
@@ -1,7 +1,7 @@
 <?php namespace Yay\tests\fixtures\parsers;
 
 use Yay\{Parser};
-use function Yay\{token, chain, buffer};
+use function Yay\{token};
 
 function my_custom_parser() : Parser {
 	return token(T_STRING);
